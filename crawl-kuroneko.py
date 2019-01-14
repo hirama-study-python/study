3e6dd132616481f90934c013eb0094ec759d2efb
平間和樹 <kh022427@gmail.com>
2019-01-14 16:51:08 +0900

    リンクの取得 -> 詳細ページから情報抽出 の処理の実装

--- crawl-kuroneko.py
+++ crawl-kuroneko.py
@@ -8,9 +8,8 @@ from time import sleep
 
 
 def main():
-    base_url = "https://www.wikipedia.org/"
-
-    url = base_url + "wiki/%E9%A6%96%E9%83%BD%E3%81%AE%E4%B8%80%E8%A6%A7"
+    base_url = "https://ja.wikipedia.org"
+    url = base_url + "/wiki/首都の一覧"
 
 
     # requestsを利用して一覧ページのURLを取得
@@ -21,25 +20,29 @@ def main():
     soup = BeautifulSoup(html, 'html.parser')
 
     # classを指定してtable情報の取得
-    country_table = soup.find_all('table', class_='wikitable sortable jquery-tsblesorter')
+    country_table = soup.find_all('table', class_='wikitable')[0]
 
-    for country_raw in country_table:
+    for country_raw in country_table.find_all("tr"):
 
-        # table情報からアンカータグ(link)を取得
-        country_links = country_raw.find_all('a')
+        country_columns = country_raw.find_all("td")
+        country_links = country_raw.find_all("a")
 
         for country_link in country_links:
-            print(f"{country_link.string}: {base_url + country_link.get('href')}")
 
-            # 詳細ページの取得
-            country_detail(country_link.string, base_url + country_link.get('href'))
+            if len(country_columns) != 0:
+                print(f" 国名 :{country_columns[2].text}")
+                print(f" 首都名　:{country_columns[1].text}")
+                print(f" URL : {base_url + country_link.get('href')}")
 
-            # 必ずsleepを入れる
-            sleep(2)
+                # 詳細ページの取得
+                country_detail( country_link.string , base_url + country_link.get( 'href' ) )
 
-            break
+                sleep( 2 )
 
-        break
+                break
+
+
+
 
 
 def country_detail(name, url):
@@ -54,14 +57,18 @@ def country_detail(name, url):
     # table要素を取得
     info_table = soup.find_all('table', class_='infobox')
 
-    # th, tdタグ要素を取得
-    english_name = info_table[0].find_all('tr')
+
+    english_name = info_table[0].find_all('small')
+
+    overviews = soup.find_all('p')
 
 
     # 該当テキスト要素の取得
     e_name = english_name[0].text
+    overview = overviews[1].text
 
+    print(f"　英名　: {e_name}")
+    print(f' 概要 : {overview}')
 
-    print(f"{name}: {e_name}")
 
 main()
