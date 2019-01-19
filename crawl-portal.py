ece84de14e5cbe7a5da48f6de2c68f2df7261241
平間和樹 <kh022427@gmail.com>
2019-01-19 22:58:51 +0900

    再微修正

--- crawl-portal.py
+++ crawl-portal.py
@@ -6,36 +6,38 @@ from time import sleep
 import re
 
 
+
 def main():
     url = "https://ja.wikipedia.org/wiki/Portal:最近の出来事"
 
-    r = requests.get( url )
+    r = requests.get(url)
     html = r.content
 
     # BeautifulSoupでHTMLをパース
-    soup = BeautifulSoup( html , 'html.parser' )
+    soup = BeautifulSoup(html, 'html.parser')
 
     # ページ内のテーブルを取ってくる
-    date_headlines_table = soup.find( 'table' )
+    date_headlines_table = soup.find('table')
 
     # 見出しの日付を取得する
-    date_headlines = date_headlines_table.find_all( 'h3' )
+    date_headlines = date_headlines_table.find_all('h3')
 
     for date_headline in date_headlines:
 
-        print( date_headline.text )
+        print(date_headline.text)
 
         # 日付見出しの次は必ず<ui>タグが設定されている
         newses = date_headline.next_sibling.string.next_sibling
 
         # <ui>タグ内の<li>タグを探す
-        for news in newses.find_all( 'li' ):
-            for category in news.find_all( "i" ):
-                re_news = re.sub( '（[^）]*）' , '' , news.text )
-                print( category.text )
-                print( re_news )
+        for news in newses.find_all('li'):
+            #<li>タグの内の<i>タグを探す
+            for category in news.find_all("i"):
+                #正規表現で（＊＊）というカテゴリ部分を削除
+                re_news = re.sub('（[^）]*）', '', news.text)
+                #カテゴリとニュース内容を出力
+                print(category.text)
+                print(re_news)
 
             sleep( 2 )
-
-
 main()
