c99de4ecad6c9df0387bc04763f69069e19754ee
平間和樹 <kh022427@gmail.com>
2019-02-16 17:47:42 +0900

    現時点での進捗共有 ・とりあえず以前作成したコードを転記 ・エラーはnext_slibingのところ

--- myfirstproject/myfirstproject/myfirstproject/spiders/myfirstspider.py
+++ myfirstproject/myfirstproject/myfirstproject/spiders/myfirstspider.py
@@ -1,11 +1,40 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 
 
 class MyfirstspiderSpider(scrapy.Spider):
     name = 'myfirstspider'
-    allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+    allowed_domains = ['ja.wikipedia.org']
+    start_urls = ['https://ja.wikipedia.org/wiki/Portal:最近の出来事']
+
 
     def parse(self, response):
-        pass
+        date_headlines_table = response.xpath("//div[@class='mw-parser-output']")
+        for date_headline in date_headlines_table.xpath("//h3"):
+            date_datail = date_headline.xpath("//span[@class='mw-headline']")
+            newses = date_headline.next_sibling.string.next_sibling
+
+            # <ui>タグ内の<li>タグを探す
+            for news in newses.xpath("//li"):
+                # カテゴリがない場合のエラー処理を実装
+                try:
+                    category = news.xpath("//i").extract()
+                except AttributeError:
+                    category = "-"
+
+                re_news = re.sub('（[^）]*）', '', news.text)
+
+                yield {
+                        "date": date_datail,
+                        "category": category,
+                        "news": re_news
+                        }
+
+
+
+
+
+
+
+
